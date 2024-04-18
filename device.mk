e35ecf108a791020ab46d87a67cfeb6187a795da
NikitaSavc <micraftur@gmail.com>
2024-04-18 22:27:00 +0300

    topaz: Build gatekeeper impl & service Fixes not starting service with error 0x20

--- device.mk
+++ device.mk
@@ -239,7 +239,9 @@ PRODUCT_COPY_FILES += \
 
 # Gatekeeper
 PRODUCT_PACKAGES += \
-    android.hardware.gatekeeper@1.0.vendor
+    android.hardware.gatekeeper@1.0.vendor \
+    android.hardware.gatekeeper@1.0-impl \
+    android.hardware.gatekeeper@1.0-service
 
 # GPS
 PRODUCT_PACKAGES += \
