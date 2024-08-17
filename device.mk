464d77156ebe1a3cf30d387b99e8ed185abef7bf
mirabelle1999 <jonzdepaz118@gmail.com>
2024-08-17 16:51:31 +0800

    topaz: remove deprecated modules in product_packages
    Signed-off-by: mirabelle1999 <jonzdepaz118@gmail.com>

--- device.mk
+++ device.mk
@@ -161,10 +161,7 @@ PRODUCT_SET_DEBUGFS_RESTRICTIONS := true
 PRODUCT_PACKAGES += \
     android.hardware.graphics.composer@2.4.vendor \
     android.hardware.graphics.mapper@4.0-impl-qti-display \
-    gralloc.bengal \
     libdisplayconfig.qti \
-    libdisplayconfig.qti.vendor \
-    libdisplayconfig.system.qti \
     libgralloc.qti \
     libqdMetaData \
     libqdMetaData.system \
@@ -336,7 +333,6 @@ PRODUCT_PACKAGES += \
     libavservices_minijail \
     libavservices_minijail.vendor \
     libcodec2_hidl@1.0.vendor \
-    libcodec2_hidl_plugin.vendor \
     libcodec2_vndk.vendor \
     libsfplugin_ccodec_utils.vendor \
     libplatformconfig
@@ -454,10 +450,8 @@ PRODUCT_PACKAGES += \
     libjsoncpp.vendor \
     libqti_vndfwk_detect.vendor \
     libqti_vndfwk_detect_vendor \
-    libqti_vndfwk_detect_vendor.vendor \
     libsqlite.vendor \
-    libvndfwk_detect_jni.qti.vendor \
-    libvndfwk_detect_jni.qti_vendor.vendor
+    libvndfwk_detect_jni.qti.vendor
 
 # QTI service tracker
 PRODUCT_PACKAGES += \
