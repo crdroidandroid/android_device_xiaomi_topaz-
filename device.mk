248a0d0dcdf493abc0e3da072c2b2432c8e6da36
Ali Hasan <ahb7671@gmail.com>
2024-10-21 16:16:46 +0800

    topaz: Use prebuilt audioservice
    Change-Id: I52ecfbbb03c29779d791d966875b5dc3a803a5c4
    Signed-off-by: Ali Hasan <ahb7671@gmail.com>

--- device.mk
+++ device.mk
@@ -46,7 +46,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.audio@7.0-impl \
     android.hardware.audio.effect@7.0-impl \
-    android.hardware.audio.service \
     android.hardware.soundtrigger@2.3-impl
 
 PRODUCT_PACKAGES += \
